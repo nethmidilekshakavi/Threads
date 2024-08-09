2527a31d5279d054ed049d0fb0d9bbf95a34b62f
nethmidilekshakavi <nethmidileksha6@gmail.com>
2024-08-09 20:59:16 +0530

    Lamda with Threads

--- Lamda/src/Main.java
+++ Lamda/src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Runnable one = () ->{
+            for (int i =1; i<=5; i++){
+                System.out.println("one");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        Runnable two = () ->{
+            for (int i =1; i<=5; i++){
+                System.out.println("two");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+
+        Thread t1 = new Thread(one);
+        Thread t2 = new Thread(two);
+
+
+        t1.start();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        t2.start();
+
     }
 }
